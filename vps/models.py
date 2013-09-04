8caf4314d64293c1042f3375b0e56eed01155ffc
Mina Nagy Zaki <mnzaki@gmail.com>
2013-09-04 23:28:14 +0200

    Initial commit

---
+++ vps/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from djmoney.models.fields import MoneyField
+
+class Flavor(models.Model):
+    name_en = models.CharField(max_length=100, null=True)
+    name_ar = models.CharField(max_length=100, null=True, blank=True)
+    ram_mb = models.IntegerField()
+    disk_gb = models.IntegerField()
+
+class Plan(models.Model):
+    months = models.IntegerField()
+    flavor = models.ForeignKey(Flavor)
+    amount = MoneyField(max_digits=10, decimal_places=2, default_currency='EGP')
+
+class VPS(models.Model):
+    owner = models.ForeignKey(User)
+    plan = models.ForeignKey(Plan)
+    instance_id = models.CharField(max_length=32)
+    ip = models.CharField(max_length=15)
