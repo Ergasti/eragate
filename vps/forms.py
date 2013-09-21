0b553c5c8a2d73b438aebeac894fb728284cefca
BeshoSmile <beshosmile@gmail.com>
2013-09-21 18:46:57 +0200

    Fix some styling issues and some langauge issues

--- vps/forms.py
+++ vps/forms.py
@@ -20,23 +20,24 @@ class RegistrationForm(forms.ModelForm):
         widgets = {
             # 'email': forms.TextInput(attrs = {'placeholder': 'Email example@example.com','type':'email'}),
             # 'name' : forms.TextInput(attrs = {'placeholder': 'Full Name'}),
-            'phone_number' : forms.TextInput(attrs = {'type': 'number'}),
+            'phone_number' : forms.TextInput(attrs = {'placeholder': 'Mobile ','type': 'number','class':'form-control'}),
             'gender' : forms.Select(choices=gender_choices),
-            'date_Of_birth' : forms.DateInput(attrs = {'type': 'date'}),
+            'date_Of_birth' : forms.DateInput(attrs = {'type': 'date','class':'form-control'}),
         }
 
 class UserCreateForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    password1 = forms.RegexField(label=("Password"),widget=forms.PasswordInput(attrs={'placeholder': 'Enter your Password ','onkeyup': 'passwordStrength(this.value)','class':'input-xlarge'}),regex=r'^.*(?=.{6,})(?=.*[a-z]).*$')
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'placeholder': 'ReEnter password ','class':'input-xlarge'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email','class':'form-control','type':'text'}))
+    username=forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'username','class':'form-control','type':'text'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email','class':'form-control','type':'email'}))
+    password1 = forms.RegexField(label=("Password"),widget=forms.PasswordInput(attrs={'placeholder': 'Enter your Password ','onkeyup': 'passwordStrength(this.value)','class':'form-control'}),regex=r'^.*(?=.{6,})(?=.*[a-z]).*$')
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'placeholder': 'ReEnter password ','class':'form-control'}))
 
     class Meta:
         model = User
         fields = ("first_name","last_name","email", "password1", "password2")
         widgets = {
-            "first_name" : forms.TextInput(attrs = {'type': 'text'}),
-            "last_name" : forms.TextInput(attrs = {'type': 'text'}),
+            "first_name" : forms.TextInput(attrs = {'type': 'text','class':'form-control'}),
+            "last_name" : forms.TextInput(attrs = {'type': 'text','class':'form-control'}),
 
         }
 
